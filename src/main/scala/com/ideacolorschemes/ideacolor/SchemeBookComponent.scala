bf3a6157890af4fdfc71d3cf19f8edeaaedfd188
iron9light <iron9light@gmail.com>
2012-02-20 19:29:33 +0800

    Run initUpdate just after app starting.

--- src/main/scala/com/ideacolorschemes/ideacolor/SchemeBookComponent.scala
+++ src/main/scala/com/ideacolorschemes/ideacolor/SchemeBookComponent.scala
@@ -3,16 +3,22 @@ package com.ideacolorschemes.ideacolor
 import util.Loggable
 import com.intellij.openapi.editor.colors.{EditorColorsScheme, EditorColorsListener}
 import com.intellij.openapi.components.{ServiceManager, ApplicationComponent}
+import com.intellij.openapi.progress.Task.Backgroundable
+import com.intellij.openapi.progress.{ProgressIndicator, ProgressManager}
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.ide.AppLifecycleListener
 
 /**
-* @author il
-*/
+ * @author il
+ */
 class SchemeBookComponent extends ApplicationComponent with Loggable {
   val schemeBookManager = ServiceManager.getService(classOf[SchemeBookManager])
+
   import schemeBookManager._
 
   def initComponent() {
-    editorColorsManager.addEditorColorsListener(new EditorColorsListener{
+    editorColorsManager.addEditorColorsListener(new EditorColorsListener {
       def globalSchemeChange(scheme: EditorColorsScheme) {
         val name = scheme.getName
         if (contains(name)) {
@@ -23,8 +29,17 @@ class SchemeBookComponent extends ApplicationComponent with Loggable {
 
     UserManager.subscribe(schemeBookManager)
 
-    // todo: run async
-    initUpdate()
+    val listener: AppLifecycleListener = new AppLifecycleListener.Adapter {
+      override def appStarting(projectFromCommandLine: Project) {
+        ProgressManager.getInstance().run(new Backgroundable(projectFromCommandLine, "Update color schemes", false) {
+          def run(indicator: ProgressIndicator) {
+            initUpdate()
+          }
+        })
+      }
+    }
+
+    ApplicationManager.getApplication.getMessageBus.connect().subscribe(AppLifecycleListener.TOPIC, listener)
   }
 
   def disposeComponent() {
