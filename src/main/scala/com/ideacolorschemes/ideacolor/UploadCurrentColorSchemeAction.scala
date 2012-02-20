c5e5421588e0e046714c85f323100b39af448231
iron9light <iron9light@gmail.com>
2012-02-20 23:07:05 +0800

    rename UploadCurrentColorSchemeAction

--- src/main/scala/com/ideacolorschemes/ideacolor/UploadCurrentColorSchemeAction.scala
+++ src/main/scala/com/ideacolorschemes/ideacolor/UploadCurrentColorSchemeAction.scala
@@ -10,7 +10,7 @@ import com.intellij.openapi.ui.Messages
  * @author il
  */
 
-class UploadCurrentColorScheme extends AnAction {
+class UploadCurrentColorSchemeAction extends AnAction {
   def ideaEditorColorsManager = EditorColorsManager.getInstance
   
   def actionPerformed(anActionEvent: AnActionEvent) {
